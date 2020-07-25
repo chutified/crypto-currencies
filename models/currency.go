2d163a901a4d6c23ee839a36472faab4173aaafe
Tommy Chu <tommychu2256@gmail.com>
2020-07-25 20:55:33 +0200

    add currency model

---
+++ models/currency.go
@@ -0,0 +1,13 @@
+package models
+
+// Currency defines a basic data model for the cryptocurrency.
+type Currency struct {
+	Name              string
+	Symbol            string
+	MarketCapUSD      float64
+	Price             float64
+	CirculatingSupply float64
+	ChangeHour        float32
+	ChangeDay         float32
+	ChangeWeek        float32
+}
