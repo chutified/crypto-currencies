ddb938a85dd9f7be0345f06824c7282be5158ae2
Tommy Chu <tommychu2256@gmail.com>
2020-07-26 11:01:29 +0200

    add comments

--- data/symbols.go
+++ data/symbols.go
@@ -2,8 +2,10 @@ package data
 
 import models "github.com/chutified/crypto-currencies/models"
 
+// symbolsConv defines a map of symbols and crypto currency names.
 type symbolsConv map[string]string
 
+// fromCurrencies updates the symbolsConv with the map of name and currency model.
 func (cs symbolsConv) fromCurrencies(ccs map[string]*models.Currency) {
 
 	// construct
