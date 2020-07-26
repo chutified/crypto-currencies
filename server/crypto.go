c21349f455006b831cd1de5c0ce1c83bc71e60ee
Tommy Chu <tommychu2256@gmail.com>
2020-07-26 10:35:33 +0200

    split handler and server

--- server/crypto.go
+++ server/crypto.go
@@ -3,7 +3,6 @@ package server
 import (
 	"context"
 	"log"
-	"strings"
 
 	"github.com/chutified/crypto-currencies/data"
 	"github.com/chutified/crypto-currencies/protos/crypto"
@@ -36,32 +35,5 @@ func (c *Crypto) GetCrypto(ctx context.Context, req *crypto.GetCryptoRequest) (*
 	}
 
 	// success
-	return resp, nil
-}
-
-func (c *Crypto) handleGetCryptoRequest(req *crypto.GetCryptoRequest) (*crypto.GetCryptoResponse, error) {
-
-	// get name
-	name := req.GetName()
-	name = strings.ToUpper(name)
-	// get currency
-	crc, err := c.ds.GetCurrency(name)
-	if err != nil {
-		return nil, errors.Wrap(err, "call data service GetCurrency")
-	}
-
-	// construct response
-	resp := &crypto.GetCryptoResponse{
-		Name:              crc.Name,
-		Symbol:            crc.Symbol,
-		MarketCapUSD:      crc.MarketCapUSD,
-		Price:             crc.Price,
-		CirculatingSupply: crc.CirculatingSupply,
-		Mineable:          crc.Mineable,
-		Volume:            crc.Volume,
-		ChangeHour:        crc.ChangeHour,
-		ChangeDay:         crc.ChangeDay,
-		ChangeWeek:        crc.ChangeWeek,
-	}
 	return resp, nil
 }
---
+++ server/handlers.go
@@ -0,0 +1,35 @@
+package server
+
+import (
+	"strings"
+
+	"github.com/chutified/crypto-currencies/protos/crypto"
+	"github.com/pkg/errors"
+)
+
+func (c *Crypto) handleGetCryptoRequest(req *crypto.GetCryptoRequest) (*crypto.GetCryptoResponse, error) {
+
+	// get name
+	name := req.GetName()
+	name = strings.ToUpper(name)
+	// get currency
+	crc, err := c.ds.GetCurrency(name)
+	if err != nil {
+		return nil, errors.Wrap(err, "call data service GetCurrency")
+	}
+
+	// construct response
+	resp := &crypto.GetCryptoResponse{
+		Name:              crc.Name,
+		Symbol:            crc.Symbol,
+		MarketCapUSD:      crc.MarketCapUSD,
+		Price:             crc.Price,
+		CirculatingSupply: crc.CirculatingSupply,
+		Mineable:          crc.Mineable,
+		Volume:            crc.Volume,
+		ChangeHour:        crc.ChangeHour,
+		ChangeDay:         crc.ChangeDay,
+		ChangeWeek:        crc.ChangeWeek,
+	}
+	return resp, nil
+}
