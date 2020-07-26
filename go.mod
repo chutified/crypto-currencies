2cf82c89bc9f38af5f594b33523351dcd75f4c14
Tommy Chu <tommychu2256@gmail.com>
2020-07-26 10:29:32 +0200

    add GetCryptoRequest handler

--- go.mod
+++ go.mod
@@ -4,5 +4,8 @@ go 1.14
 
 require (
 	github.com/PuerkitoBio/goquery v1.5.1
+	github.com/golang/protobuf v1.4.2
 	github.com/pkg/errors v0.9.1
+	google.golang.org/grpc v1.30.0
+	google.golang.org/protobuf v1.25.0
 )
--- server/crypto.go
+++ server/crypto.go
@@ -2,8 +2,11 @@ package server
 
 import (
 	"log"
+	"strings"
 
 	"github.com/chutified/crypto-currencies/data"
+	"github.com/chutified/crypto-currencies/protos/crypto"
+	"github.com/pkg/errors"
 )
 
 type Crypto struct {
@@ -18,4 +21,35 @@ func New(log *log.Logger, ds *data.Service) *Crypto {
 	}
 
 	return c
+}
+
+// func (c *Crypto) GetCrypto(ctx context.Context, req *crypto.GetCryptoRequest) (*crypto.GetCryptoResponse, error) {
+
+// }
+
+func (c *Crypto) handleGetCryptoRequest(req *crypto.GetCryptoRequest) (*crypto.GetCryptoResponse, error) {
+
+	name := req.GetName()
+	name = strings.ToUpper(name)
+	// get currency
+	crc, err := c.ds.GetCurrency(name)
+	if err != nil {
+		return nil, errors.Wrap(err, "call data service GetCurrency")
+	}
+
+	// construct response
+	resp := &crypto.GetCryptoResponse{
+		Name:              crc.Name,
+		Symbol:            crc.Symbol,
+		MarketCapUSD:      crc.MarketCapUSD,
+		Price:             crc.Price,
+		CirculatingSupply: crc.CirculatingSupply,
+		Mineable:          crc.Mineable,
+		Volume:            crc.Volume,
+		ChangeHour:        crc.ChangeHour,
+		ChangeDay:         crc.ChangeDay,
+		ChangeWeek:        crc.ChangeWeek,
+	}
+
+	return resp, nil
 }
