6b06073edf955e52feae98b3b41dd0207b137e1b
Tommy Chu <tommychu2256@gmail.com>
2020-07-26 08:27:15 +0200

    add update

--- data/cryptos.go
+++ data/cryptos.go
@@ -1,6 +1,9 @@
 package data
 
-import models "github.com/chutified/crypto-currencies/models"
+import (
+	models "github.com/chutified/crypto-currencies/models"
+	"github.com/pkg/errors"
+)
 
 type Service struct {
 	Currencies map[string]*models.Currency
@@ -10,4 +13,23 @@ func New() *Service {
 	return &Service{
 		Currencies: make(map[string]*models.Currency),
 	}
+}
+
+func (s *Service) Update() error {
+
+	// fetch
+	rs, err := fetchRecords("https://coinmarketcap.com/all/views/all/")
+	if err != nil {
+		return errors.Wrap(err, "fetching records")
+	}
+
+	// parse records
+	ccs, err := parseRecords(rs)
+	if err != nil {
+		return errors.Wrap(err, "parsing records")
+	}
+
+	// update
+	s.Currencies = ccs
+	return nil
 }
