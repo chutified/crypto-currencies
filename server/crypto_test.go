6eb7a2b8b49b07a3cbbb268b6fd668744e923175
Tommy Chu <tommychu2256@gmail.com>
2020-07-28 20:18:28 +0200

    add test for GetCrypto

--- server/crypto_test.go
+++ server/crypto_test.go
@@ -2,10 +2,14 @@ package server
 
 import (
 	"bytes"
+	"context"
+	"fmt"
 	"log"
 	"testing"
 
 	"github.com/chutified/crypto-currencies/data"
+	"github.com/chutified/crypto-currencies/protos/crypto"
+	"gopkg.in/go-playground/assert.v1"
 )
 
 func TestCrypto(t *testing.T) {
@@ -25,5 +29,35 @@ func TestCrypto(t *testing.T) {
 		name     string
 		currName string
 		err      string
-	}{}
+	}{
+		{
+			name:     "ok",
+			currName: "BITCOIN",
+			err:      "",
+		},
+		{
+			name:     "not found",
+			currName: "invalid",
+			err:      "cryptocurrency '.*' not found",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t1 *testing.T) {
+
+			resp, err := c.GetCrypto(context.Background(), &crypto.GetCryptoRequest{
+				Name: test.currName,
+			})
+			if err != nil {
+
+				exp := fmt.Sprintf("%s.*", test.err)
+				assert.MatchRegex(t1, err.Error(), exp)
+
+			} else {
+
+				assert.Equal(t1, "", test.err)
+				assert.Equal(t1, resp.Name, test.currName)
+			}
+		})
+	}
 }
