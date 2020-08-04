c560c2e3cb15fa86c71064a2f302ff4975b65305
Tommy Chu <tommychu2256@gmail.com>
2020-08-04 20:25:58 +0200

    literally nothing

--- protos/crypto/crypto.pb.go
+++ protos/crypto/crypto.pb.go
@@ -8,6 +8,9 @@ package crypto
 
 import (
 	context "context"
+	reflect "reflect"
+	sync "sync"
+
 	proto "github.com/golang/protobuf/proto"
 	status "google.golang.org/genproto/googleapis/rpc/status"
 	grpc "google.golang.org/grpc"
@@ -15,8 +18,6 @@ import (
 	status1 "google.golang.org/grpc/status"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
-	reflect "reflect"
-	sync "sync"
 )
 
 const (
